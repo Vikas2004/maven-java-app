425866b797970e9f8ff868580aa2b4c3016332f4
Vikas2004 <S538336@NWMISSOURI.EDU>
2021-02-19 14:28:02 -0600

    add maven archetype

---
+++ src/main/java/com/baswapuram/vikas/bigdata/App.java
@@ -0,0 +1,13 @@
+package com.baswapuram.vikas.bigdata;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hi! This is Vikas and I have created the Maven java application using the Powershell." );
+    }
+}
